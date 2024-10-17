9637ba146b35cbaaf288697dfceae53a08b10175
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2024-10-17 13:44:13 -0700

    can print a full board, and have test cases, move on to populating the board

--- base_game.js
+++ base_game.js
@@ -1,36 +1,60 @@
-function print_row(row){
-    if(!row){
+function getMaxNumberLength(board) {
+    let maxNum = Math.max(...board.flat().filter(num => num !== null && num !== ''));
+    return maxNum.toString().length;
+}
+
+function print_row(row, maxLength = 1) {
+    if (!row) {
         return -1;
     }
 
-    let row_out =   "## ";
-    for(let i in row){
-        row_out += row[i];
-        row_out  += ' ';
+    let row_out = "# ";
+    for (let i in row) {
+        // Dynamically pad each number based on the length of the largest number
+        let value = row[i] !== null && row[i] !== '' ? row[i].toString() : '-';
+        row_out += value.padStart(maxLength, ' ');
+        row_out += ' ';
     }
-    row_out  += '##';
+    row_out += '#';
 
     console.log(row_out);
 }
 
-function print_border(){
-    let border  = "#".repeat(13);
+function print_border(maxLength = 1) {
+    let totalLength = (maxLength + 1) * 4 + 3;  // Calculate border based on row length
+    let border = "#".repeat(totalLength);
     console.log(border);
 }
 
-function print_board(board){
-    if(!board){
+function print_board(board) {
+    if (!board) {
         return -1;
     }
 
-    print_border();
-    print_border();
+    let maxLength = getMaxNumberLength(board);
+
+    print_border(maxLength);
+    for (let row in board) {
+        print_row(board[row], maxLength);
+    }
+    print_border(maxLength);
 }
 
+let board = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+];
 
-let row = [1,2,3,4];
-print_row(row);
+let board2 = [
+    [1, 2, null, 4],
+    [5, 6, null, 8],
+    [null, 10, 11, 12],
+    [13, null, 15, 16]
+];
 
+print_board(board)
 
 module.exports = {
     print_row,
--- test_base_game.js
+++ test_base_game.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const sinon = require('sinon');
-const { print_border, print_row } = require('./base_game');
+const { print_border, print_row, print_board } = require('./base_game');
 
 describe('Base Game Functions', function() {
     let consoleSpy;
@@ -15,19 +15,44 @@ describe('Base Game Functions', function() {
         consoleSpy.restore();
     });
 
-    it('should print a border of 13 # characters', function() {
+    it('should print a border of 11 # characters', function() {
         // Call the function
         print_border();
 
         // Assert that console.log was called with the correct argument
-        assert(consoleSpy.calledWithExactly('#############'));
+        assert(consoleSpy.calledWithExactly('###########'));
     });
 
-    it('should print row as ## 1 2 3 4 ##', function() {
+    it('should print row as # 1 2 3 4 #', function() {
         // Call the function with a test row
         print_row([1, 2, 3, 4]);
 
         // Assert that console.log was called with the correct formatted output
-        assert(consoleSpy.calledWithExactly('## 1 2 3 4 ##'));
+        assert(consoleSpy.calledWithExactly('# 1 2 3 4 #'));
     });
+    
+    it('should print simple board', function() {
+        let board = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ];
+    
+        print_board(board);
+    
+        const correct_board = [
+            '###############',
+            '#  1  2  3  4 #',
+            '#  5  6  7  8 #',
+            '#  9 10 11 12 #',
+            '# 13 14 15 16 #',
+            '###############'
+        ];
+    
+        correct_board.forEach((line, index) => {
+            assert(consoleSpy.getCall(index).calledWithExactly(line));
+        });
+    });
+    
 });
